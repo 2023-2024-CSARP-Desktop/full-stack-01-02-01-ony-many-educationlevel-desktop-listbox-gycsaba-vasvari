75a9f0534b2721c2b63c93ecc6eccdc9cd0cd710
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-20 10:29:06 +0100

    Service és dependency injection

--- Kreta.Desktop/ViewModels/Administration/EducationLevelViewModel.cs
+++ Kreta.Desktop/ViewModels/Administration/EducationLevelViewModel.cs
@@ -1,9 +1,17 @@
 ﻿using Kreta.Desktop.ViewModels.Base;
+using Kreta.HttpService.Services;
 
 namespace Kreta.Desktop.ViewModels.Administration
 {
     public partial class EducationLevelViewModel : BaseViewModel
     {
+        private readonly IEducationLevelService? _educationLevelService;
+
+        public EducationLevelViewModel(IEducationLevelService? educationLevelService)
+        {
+            _educationLevelService = educationLevelService;
+        }
+
         public string Title { get; set; } = "Tanulmányi szint kezelése";
     }
 }

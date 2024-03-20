a89c4cbba7cb0113eb74c72111c3d0d0518939a5
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-20 12:00:03 +0100

    InitializeAsync metódus

--- Kreta.Desktop/ViewModels/Administration/EducationLevelViewModel.cs
+++ Kreta.Desktop/ViewModels/Administration/EducationLevelViewModel.cs
@@ -3,6 +3,7 @@ using Kreta.Desktop.ViewModels.Base;
 using Kreta.HttpService.Services;
 using Kreta.Shared.Models;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace Kreta.Desktop.ViewModels.Administration
 {
@@ -16,12 +17,21 @@ namespace Kreta.Desktop.ViewModels.Administration
         [ObservableProperty]
         private EducationLevel _selectedEducationLevel = new();
 
+        public EducationLevelViewModel()
+        {            
+        }
         public EducationLevelViewModel(IEducationLevelService? educationLevelService)
         {
             _educationLevelService = educationLevelService;
         }
 
         public string Title { get; set; } = "Tanulmányi szint kezelése";
+
+        public override async Task InitializeAsync()
+        {
+            await UpdateView();
+            await base.InitializeAsync();
+        }
 
 
     }

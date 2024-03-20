578fc568262026f0773acab34e3e8fb4c36bf01c
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-20 10:31:28 +0100

    A ListBox property és a kijelölt tanulmányi szint property

--- Kreta.Desktop/ViewModels/Administration/EducationLevelViewModel.cs
+++ Kreta.Desktop/ViewModels/Administration/EducationLevelViewModel.cs
@@ -1,5 +1,8 @@
-﻿using Kreta.Desktop.ViewModels.Base;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Kreta.Desktop.ViewModels.Base;
 using Kreta.HttpService.Services;
+using Kreta.Shared.Models;
+using System.Collections.ObjectModel;
 
 namespace Kreta.Desktop.ViewModels.Administration
 {
@@ -7,11 +10,19 @@ namespace Kreta.Desktop.ViewModels.Administration
     {
         private readonly IEducationLevelService? _educationLevelService;
 
+        [ObservableProperty]
+        private ObservableCollection<EducationLevel> _educationLevels = new();
+
+        [ObservableProperty]
+        private EducationLevel _selectedEducationLevel = new();
+
         public EducationLevelViewModel(IEducationLevelService? educationLevelService)
         {
             _educationLevelService = educationLevelService;
         }
 
         public string Title { get; set; } = "Tanulmányi szint kezelése";
+
+
     }
 }
